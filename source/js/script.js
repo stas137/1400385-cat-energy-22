15a5b0cc9e9f391220300d23775798db17af3530
stas137 <stasccb@gmail.com>
2021-05-16 16:43:08 +0300

    Декоративные элементы главной страницы

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,20 @@
-document.addEventListener('DOMContentLoaded', ready);
+const mainNav = document.querySelector('.main-nav');
+const mainNavButton = document.querySelector('.main-nav-button');
+const mainNavToggle = document.querySelector('.main-nav-button__toggle');
 
-function ready() {
-  let pageContainer = document.querySelector('.page-header__container');
-  let mainNav = document.querySelector('.main-nav');
-  let mainNavToggle = document.querySelector('.main-nav__toggle');
+mainNavButton.classList.remove('main-nav-button--nojs');
 
-  pageContainer.classList.remove('main-nav--nojs');
+mainNavToggle.addEventListener('click', () => {
+  if (mainNav.classList.contains('main-nav--closed')){
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--opened');
+    mainNavButton.classList.remove('main-nav-button--closed');
+    mainNavButton.classList.add('main-nav-button--opened');
+  } else {
+    mainNav.classList.add('main-nav--closed');
+    mainNav.classList.remove('main-nav--opened');
+    mainNavButton.classList.add('main-nav-button--closed');
+    mainNavButton.classList.remove('main-nav-button--opened');
+  }
+});
 
-  mainNavToggle.addEventListener('click', () => {
-    if (mainNav.classList.contains('main-nav--closed')){
-      mainNav.classList.remove('main-nav--closed');
-      mainNav.classList.add('main-nav--opened');
-      pageContainer.classList.remove('main-nav--closed');
-      pageContainer.classList.add('main-nav--opened');
-    } else {
-      mainNav.classList.add('main-nav--closed');
-      mainNav.classList.remove('main-nav--opened');
-      pageContainer.classList.add('main-nav--closed');
-      pageContainer.classList.remove('main-nav--opened');
-    }
-  });
-}
